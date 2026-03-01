89f67e417d7eb69edf1991c119f9b53a46c6804c
Kardian76 <kardian@gmail.com>
2026-02-28 23:38:25 -0500

    Bump version from v5 to v6

--- snowball-sw.js
+++ snowball-sw.js
@@ -1,5 +1,5 @@
 // ===== VERSIONING =====
-const VERSION = 'v5'; // Bump this on every deploy
+const VERSION = 'v6'; // Bump this on every deploy
 const STATIC_CACHE = `static-${VERSION}`;
 const APP_SHELL = [
   '/',
