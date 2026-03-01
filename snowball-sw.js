0f73e02560701d449fdb82a20edbe716b5dd6458
Kardian76 <kardian@gmail.com>
2026-02-28 21:37:34 -0500

    Bump version from v3 to v4

--- snowball-sw.js
+++ snowball-sw.js
@@ -1,5 +1,5 @@
 // ===== VERSIONING =====
-const VERSION = 'v3'; // Bump this on every deploy
+const VERSION = 'v4'; // Bump this on every deploy
 const STATIC_CACHE = `static-${VERSION}`;
 const APP_SHELL = [
   '/',
