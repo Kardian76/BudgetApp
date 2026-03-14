0b42833c35c8ac2fa2c965345a54c9309ac038c6
Kardian76 <kardian@gmail.com>
2026-03-14 19:14:09 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker for Purrfect Budget PWA */
-const CACHE_NAME = 'purrfect-budget-v17';
+const CACHE_NAME = 'purrfect-budget-v18';
 const OFFLINE_URL = 'index.html';
 
 // Files to cache for offline use
