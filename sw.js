6d3f62fe565b06cd787bae9a54591f9cee4937da
Kardian76 <kardian@gmail.com>
2026-04-28 20:53:53 -0400

    Update CACHE_NAME to version v38

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker for Purrfect Budget PWA */
-const CACHE_NAME = 'purrfect-budget-v37';
+const CACHE_NAME = 'purrfect-budget-v38';
 const OFFLINE_URL = 'index.html';
 
 // Files to cache for offline use
